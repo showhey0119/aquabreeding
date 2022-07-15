b76cfd8e658c3c6a0944548e39a1c91fdfdcf6d4
Shohei Takuno <takuno@21-0696.local>
2022-07-15 11:23:26 +0900

    initial project version

---
+++ aquabreeding/__init__.py
@@ -0,0 +1,1 @@
+from aquabreeding.aquabreeding import *
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='aquabreeding',
+    version='0.3.0',
+    packages=find_packages()
+)
+
+
+
+
+
+
+
+
+
+
+
+
