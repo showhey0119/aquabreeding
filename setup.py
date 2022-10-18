54e978382a5a2c6ec42374d74af5376c6a004fca
showhey0119 <showhey0119@yahoo.co.jp>
2022-10-18 11:29:07 +0900

    edit setup.py

--- setup.py
+++ setup.py
@@ -133,7 +133,7 @@ setup(
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
     extras_require={"test": ["pytest>=6.0"]},
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=[
         'numpy',
         'scipy',
